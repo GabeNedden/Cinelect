3984928509aba750f205cbff84722e562cf62e3b
GabeNedden <gabenedden@gmail.com>
2020-06-14 19:06:07 -0400

    add dropdown search

--- index.js
+++ index.js
@@ -6,13 +6,43 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    if(response.data.Error){
+        return [];
+    }
+
+    return response.data.Search;
 };
 
-const input = document.querySelector('input');
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+    <label><b>Search for a Movie!</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
-const onInput = event => {
-    fetchData(event.target.value);
+const input = document.querySelector('input');
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    resultsWrapper.innerHTML = ''
+    dropdown.classList.add("is-active");
+
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        option.classList.add("dropdown-item");
+        option.innerHTML = `
+        <img src="${movie.Poster}">
+        ${movie.Title}
+        `;
+        resultsWrapper.appendChild(option);
+    }
 };
 
 input.addEventListener('input', debounce(onInput, 800));
